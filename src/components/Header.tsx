20dc4da70aa57b215b860b3cda67020872bbceca
Franmeza <ing.meza.francisco@gmail.com>
2024-04-19 12:13:48 -0500

    animation implemented for active link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 const routes = [
   {
@@ -8,11 +13,12 @@ const routes = [
   },
   {
     name: "All Events",
-    path: "/event/all",
+    path: "/events/all",
   },
 ];
 
 function Header() {
+  const activePathname = usePathname();
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Image
@@ -21,14 +27,27 @@ function Header() {
         width={53}
         height={12}
       />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 text-sm h-full">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white transition cursor-pointer"
+              //clsx is an alternative to add styles conditionally
+              className={clsx(
+                " hover:text-white transition flex items-center cursor-pointer relative",
+                {
+                  "text-white": activePathname === route.path,
+                  "text-white/50": activePathname !== route.path,
+                }
+              )}
             >
-              <Link href={route.path} /> {route.name}
+              <Link href={route.path}> {route.name} </Link>
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="nav-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
