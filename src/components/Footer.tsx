f21432b8e6a3c3ceb44106c1cf8d9f4c40bc7f88
Franmeza <ing.meza.francisco@gmail.com>
2024-04-18 19:29:04 -0500

    Home page, header and footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex items-center mt-auto h-16 border-t border-white/10 px-3 sm:px-9  text-white/25">
+      <small className="text-xs">Implemented by Francisco Meza</small>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/event/all",
+  },
+];
 
 function Header() {
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <Link href="/event/all"> All Events</Link>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        alt="EVENTO logo"
+        width={53}
+        height={12}
+      />
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 hover:text-white transition cursor-pointer"
+            >
+              <Link href={route.path} /> {route.name}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
